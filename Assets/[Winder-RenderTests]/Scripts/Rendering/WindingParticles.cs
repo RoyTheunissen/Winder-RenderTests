2697c0f80ab792e784f545c81c360a9a82357822
RoyTheunissen <roy.theunissen@live.nl>
2019-11-03 17:52:14 +0100

    Made the winding circle have a configurable radius.

--- Assets/[Winder-RenderTests]/Scripts/Rendering/WindingParticles.cs
+++ Assets/[Winder-RenderTests]/Scripts/Rendering/WindingParticles.cs
@@ -9,9 +9,19 @@ namespace RoyTheunissen.Winder.Rendering
     {
         [SerializeField] private new ParticleSystem particleSystem;
         [SerializeField] private Transform target;
+        
+        [Space]
         [SerializeField] private float lifetimeNormalizedMin = 0.4f;
         [SerializeField] private float lifetimePerDistance = 4;
         [SerializeField] private float lifetimeExtra = 1;
+        
+        [Space]
+        [SerializeField, Range(0.5f, 5.0f)] private float coreSize = 0.5f;
+        [SerializeField, Range(0.0f, 5.0f)] private float farSizeExtra = 1.5f;
+        [SerializeField] private Transform forceFieldContainer;
+        [SerializeField] private ParticleSystemForceField forceFieldRange;
+        [SerializeField] private ParticleSystemForceField forceFieldVortexCore;
+        [SerializeField] private ParticleSystemForceField forceFieldVortexFar;
 
         private void Update()
         {
@@ -23,6 +33,20 @@ namespace RoyTheunissen.Winder.Rendering
             startLifetime.constantMax = distance * lifetimePerDistance + lifetimeExtra;
             startLifetime.constantMin = startLifetime.constantMax * lifetimeNormalizedMin;
             mainModule.startLifetime = startLifetime;
+
+            forceFieldVortexCore.endRange = coreSize;
+            forceFieldVortexFar.endRange = coreSize + farSizeExtra;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (forceFieldContainer != null)
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawWireSphere(forceFieldContainer.position, coreSize);
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawWireSphere(forceFieldContainer.position, coreSize + farSizeExtra);
+            }
         }
     }
 }
